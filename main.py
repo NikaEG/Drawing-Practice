4acf6a33191332484bd86a5e1eaea276ec4a7547
NikaEG <ally.via@gmail.com>
2020-07-22 13:45:50 +0000

    Finished

---
+++ main.py
@@ -0,0 +1,10 @@
+342import math
+
+def calc(number1, number2):
+  return number1 + number2
+
+number1 = int(input("Write a number: "))
+number2 = int(input("Write another number: "))
+
+End = calc(number1, number2)
+print("The sum is",End)
